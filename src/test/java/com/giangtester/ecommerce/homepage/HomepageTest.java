b608f2b9822ef63bd9994bd706df2ac5d4a367a3
giang nguyen <giang.nguyen@niteco.se>
2020-06-18 15:12:44 +0700

    Testcase check homeslider

--- src/test/java/com/giangtester/ecommerce/homepage/HomepageTest.java
+++ src/test/java/com/giangtester/ecommerce/homepage/HomepageTest.java
@@ -3,9 +3,7 @@ package com.giangtester.ecommerce.homepage;
 import com.giangtester.ecommerce.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.pagefactory.ByChained;
 
 import java.util.List;
