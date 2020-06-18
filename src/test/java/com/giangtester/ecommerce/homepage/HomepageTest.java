e0ceabc14d0531de9b25daef1e175036d0c98872
giang nguyen <giang.nguyen@niteco.se>
2020-06-18 15:12:08 +0700

    Testcase check homeslider

--- src/test/java/com/giangtester/ecommerce/homepage/HomepageTest.java
+++ src/test/java/com/giangtester/ecommerce/homepage/HomepageTest.java
@@ -1,27 +1,32 @@
 package com.giangtester.ecommerce.homepage;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
+import com.giangtester.ecommerce.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.pagefactory.ByChained;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class HomepageTest {
-
-    @BeforeAll
-    public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-    }
+public class HomepageTest extends BaseTest {
 
     @Test
     void checkLogo() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://automationpractice.com/index.php");
         WebElement logo = driver.findElement(By.className("logo"));
         assertThat(logo.isDisplayed()).isTrue();
+    }
+
+    @Test
+    void checkSlider() {
+        List<WebElement> images = driver.findElements(
+                new ByChained(By.id("homeslider"), By.tagName("li"), By.tagName("a")));
+
+        images.forEach(
+                image -> System.out.println(image.getAttribute("href"))
+        );
     }
 }
