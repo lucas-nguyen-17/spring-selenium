6d8f0a24a23f436c9fb4f509762df5e1168e048f
giang nguyen <giang.nguyen@niteco.se>
2020-06-19 08:41:44 +0700

    Practice locator using ByIdOrName and ByAll

--- src/test/java/com/giangtester/ecommerce/homepage/HomepageTest.java
+++ src/test/java/com/giangtester/ecommerce/homepage/HomepageTest.java
@@ -4,6 +4,8 @@ import com.giangtester.ecommerce.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ByIdOrName;
+import org.openqa.selenium.support.pagefactory.ByAll;
 import org.openqa.selenium.support.pagefactory.ByChained;
 
 import java.util.List;
@@ -21,10 +23,25 @@ public class HomepageTest extends BaseTest {
     @Test
     void checkSlider() {
         List<WebElement> images = driver.findElements(
-                new ByChained(By.id("homeslider"), By.tagName("li"), By.tagName("a")));
+                new ByChained(new ByIdOrName("homeslider"), By.tagName("li"), By.tagName("a")));
 
         images.forEach(
                 image -> System.out.println(image.getAttribute("href"))
         );
+    }
+
+    @Test
+    void checkAllLinksOnPage() {
+        List<WebElement> imagesOrPictures = driver.findElements(new ByAll(By.tagName("img"), By.tagName("picture")));
+        System.out.println(String.format("Number of images: %d", imagesOrPictures.size()));
+        imagesOrPictures.forEach(
+                image -> System.out.println(image.getAttribute("src"))
+        );
+    }
+
+    @Test
+    void checkTextWithTagBr() {
+        WebElement tagH2 = driver.findElement(By.xpath("(//h2[contains(., 'EXCEPTEUR')])[1]"));
+        assertThat(tagH2.getAttribute("innerHTML")).startsWith("EXCEPTEUR").endsWith("OCCAECAT").contains("<br>");
     }
 }
